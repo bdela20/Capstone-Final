c76ad52b2ba88d727c33178b3c9ed0860b31e82a
bdela20 <benjamindelarosa20@gmail.com>
2024-05-18 20:46:35 -0400

    second commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div 
-      style={{ backgroundImage:'/images/Gran_Bar.jpeg'}} 
+      style={{ backgroundImage: `url(/images/Gran_Bar.jpeg)` }} 
       className='flex flex-row justify-center mx-auto bg-cover bg-fixed'
-      >
-        <div className='flex place-items-center h-screen'>
-          <div>
-            <Link to="/whiskeys">
-              <h1 className='p-10 bg-white bg-opacity-45 text-black rounded'> Welcome to our Handpicked Whiskey Collection 🥃</h1>
-            </Link>
-          </div>
+    >
+      <div className='flex place-items-center h-screen'>
+        <div>
+          <Link to="/whiskeys">
+            <h1 className='p-10 bg-white bg-opacity-45 text-black rounded'>
+              Welcome to our Handpicked Whiskey Collection 🥃
+            </h1>
+          </Link>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
